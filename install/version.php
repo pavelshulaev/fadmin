aa7bd9501a41d3960e30d77a6047fd8eedcac485
pavel <rover.webdev@gmail.com>
2018-08-24 16:47:34 +0500

    bx ver 1.8.11

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?
 $arModuleVersion = array(
-    "VERSION" => "1.8.10",
-    "VERSION_DATE" => "2018-08-24 09:53:02"
+    "VERSION" => "1.8.11",
+    "VERSION_DATE" => "2018-08-24 14:46:20"
 );
 ?>
